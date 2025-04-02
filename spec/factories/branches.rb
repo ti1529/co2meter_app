52f71c981b55a556b2a3bf36565e4c6d1cf0a23b
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-02 17:38:11 +0900

    支店のModelSpec作成

--- spec/factories/branches.rb
+++ spec/factories/branches.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :branch do
-    
+    name { "支店_1" }
+    workplace_type { 0 }
+    city_category { 0 }
+    postcode { "1234567" }
+    prefecture { "千葉県" }
+    city { "千葉市" }
+    address_line1 { "123" }
+    address_line2 { "ビル_1" }
+
+    association :company
   end
 end
--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Branch, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context '支店名が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, name: "")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の勤務形態が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, workplace_type: "")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の都市区分が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, city_category: "")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の郵便番号が1文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, postcode: "1")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の都道府県が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, prefecture: "")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の市町村が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        branch = FactoryBot.build(:branch, city: "")
+        expect(branch).not_to be_valid
+      end
+    end
+
+    context '支店の企業idが空文字の場合' do
+      it '支店の登録に失敗する' do
+        branch = FactoryBot.build(:branch, company_id: "")
+        expect(branch.save).to be_falsey
+      end
+    end
+
+    context '支店名、勤務形態、都市区分、都道府県、住所、企業idに値があり、郵便番号が空文字の場合' do
+      it '支店を登録できる' do
+        branch = FactoryBot.build(:branch, postcode: "")
+        expect(branch.save).to be_truthy
+      end
+    end
+
+    context '支店名、勤務形態、都市区分、都道府県、住所、企業idに値があり、郵便番号が7文字の場合' do
+      it '支店を登録できる' do
+        branch = FactoryBot.build(:branch)
+        expect(branch.save).to be_truthy
+      end
+    end
+  end
 end
