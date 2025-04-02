55337d7182a49fa1482a508f25b60dd53f983632
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-02 19:15:50 +0900

    排出係数のModelSPec作成

--- spec/factories/co2_emission_factors.rb
+++ spec/factories/co2_emission_factors.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :co2_emission_factor do
-    
+    fiscal_year { "2020" }
+    workplace_type { 0 }
+    city_category { 0 }
+    co2_emission_factor { 2 }
+    co2_emission_factor_unit { "kgCO2/人・日" }
   end
 end
--- spec/models/co2_emission_factor_spec.rb
+++ spec/models/co2_emission_factor_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Co2EmissionFactor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context '排出係数の年度が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, fiscal_year: "")
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の年度が3桁の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, fiscal_year: "202")
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の勤務形態が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, workplace_type: "")
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の都市区分が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, city_category: "")
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の排出係数（数値）が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, co2_emission_factor: "")
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の排出係数（数値）が負の値の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, co2_emission_factor: -1)
+        expect(factor).not_to be_valid
+      end
+    end
+
+    context '排出係数の排出係数（単位）が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        factor = FactoryBot.build(:co2_emission_factor, co2_emission_factor_unit: "")
+        expect(factor).not_to be_valid
+      end
+    end
+
+
+    context '排出係数の年度、勤務形態、都市区分、CO2排出係数（単位）に値があり、CO2排出係数（数値）が正の値の場合' do
+      it 'バリデーションに成功する' do
+        factor = FactoryBot.build(:co2_emission_factor)
+        expect(factor).to be_valid
+      end
+    end
+  end
 end
