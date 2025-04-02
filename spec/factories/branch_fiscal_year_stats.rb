d617ac6ba7c8d7476fa3097f01a50e347282e751
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-02 18:55:40 +0900

    支店実績のModelSpenc作成

--- spec/factories/branch_fiscal_year_stats.rb
+++ spec/factories/branch_fiscal_year_stats.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :branch_fiscal_year_stat do
-    
+    fiscal_year { "2020" }
+    annual_working_days { 240 }
+    annual_employee_count { 20 }
+
+    association :branch
+    association :updater,
+      factory: :user
   end
 end
--- spec/models/branch_fiscal_year_stat_spec.rb
+++ spec/models/branch_fiscal_year_stat_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe BranchFiscalYearStat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context '支店実績の年度が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        stat = FactoryBot.build(:branch_fiscal_year_stat, fiscal_year: "")
+        expect(stat).not_to be_valid
+      end
+    end
+
+    context '支店実績の年間勤務日数が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        stat = FactoryBot.build(:branch_fiscal_year_stat, annual_working_days: "")
+        expect(stat).not_to be_valid
+      end
+    end
+
+    context '支店実績の年間勤務日数が400日の場合' do
+      it 'バリデーションに失敗する' do
+        stat = FactoryBot.build(:branch_fiscal_year_stat, annual_working_days: 400)
+        expect(stat).not_to be_valid
+      end
+    end
+
+    context '支店実績の従業員数が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        stat = FactoryBot.build(:branch_fiscal_year_stat, annual_employee_count: "")
+        expect(stat).not_to be_valid
+      end
+    end
+
+    context '年度、年間勤務日数、従業員数に値がある場合' do
+      it '支店を登録できる' do
+        stat = FactoryBot.build(:branch_fiscal_year_stat)
+        stat.updater.save
+        stat.branch.company = stat.updater.company
+        stat.branch.save
+        expect(stat.save).to be_truthy
+      end
+    end
+  end
 end
