69b7d7f74018d2dfd672478b2cd20e0b6b01b7be
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-02 17:37:24 +0900

    ユーザのModelSpec作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "user_1" }
+    email { "user_1@email.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    admin { false }
+
+    association :company
+  end
+
+  factory :admin, class: User do
+    name { "admin_1" }
+    email { "admin_1@email.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    admin { true }
+
+    association :company
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションのテスト' do
+    context 'ユーザの名前が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, name: "")
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのメールアドレスが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, email: "")
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのメールアドレスがすでに使用されていた場合' do
+      it 'バリデーションに失敗する' do
+        FactoryBot.create(:user)
+        user_new = FactoryBot.build(:user, name: "user_new")
+        expect(user_new).not_to be_valid
+      end
+    end
+
+    context 'ユーザのパスワードが6文字未満の場合' do
+      it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, password: "pass")
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのパスワードとパスワード（確認）が一致しない場合' do
+      it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, password_confirmation: "passpass")
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザの企業idが空文字の場合' do
+      it 'ユーザの登録に失敗する' do
+        user = FactoryBot.build(:user, company_id: "")
+        expect(user.save).to be_falsey
+      end
+    end
+
+    context 'ユーザの名前、企業idに値があり、メールアドレスが使われていない値で、かつパスワードが6文字以上でパスワード（確認）と一致する場合' do
+      it 'ユーザを登録できる' do
+        user = FactoryBot.build(:user)
+        expect(user.save).to be_truthy
+      end
+    end
+  end
 end
