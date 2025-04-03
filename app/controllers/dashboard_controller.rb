7101375f1c56ced6f101d5a0a5ec57a259cd1b84
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-03 17:12:37 +0900

    CO2算定機能、ransaclkによる初期ソート

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    # @branch_fiscal_year_stats = BranchFiscalYearStat.all
+    @q = BranchFiscalYearStat.ransack(params[:q])
+    @q.sorts = [ "fiscal_year asc", "branch_id asc" ] if @q.sorts.empty?
+    @branch_fiscal_year_stats = @q.result(distinct: true).joins(:branch).includes(:branch)
   end
 end
--- app/models/branch_fiscal_year_stat.rb
+++ app/models/branch_fiscal_year_stat.rb
@@ -5,4 +5,12 @@ class BranchFiscalYearStat < ApplicationRecord
   validates :fiscal_year, presence: true, length: { is: 4 }
   validates :annual_working_days, presence: true, numericality: { less_than_or_equal_to: 365 }
   validates :annual_employee_count, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "fiscal_year", "branch_id" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "branch" ]
+  end
 end
