02ff32d2019b73f1f19956ac34cdc1991e570a2b
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-06 17:45:31 +0900

    棒グラフの表示

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link startbootstrap/styles.css
+//= link startbootstrap/scripts.js
+//= link startbootstrap/chart-bar-demo.js
+//= link startbootstrap/datatables-demo.js
---
+++ app/assets/javascripts/startbootstrap/chart-bar-demo.js
@@ -0,0 +1,58 @@
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+const co2canvas = document.getElementById("Co2ChartBar");
+
+const labels = JSON.parse(co2canvas.dataset.labels);
+const values = JSON.parse(co2canvas.dataset.values);
+
+const ctx = co2canvas.getContext('2d');
+
+
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: "CO2排出量(ton)",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: values,
+    }],
+  },
+  options: {
+    // responsive: false,
+    // maintainAspectRatio: false,
+  
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6,
+          callback: function(value, index, values){
+            return  value +  '年度'
+          }
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 6,
+          callback: function(value, index, values){
+            return  value +  'tCO2'}
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
