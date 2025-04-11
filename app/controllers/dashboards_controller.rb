5f3d9873e316416e02a9d69e5ab71d197f133b22
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-11 09:24:21 +0900

    支店実績一覧に検索、ソート機能を実装

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,13 +1,13 @@
 class DashboardsController < ApplicationController
   def index
     @branches = current_user.company.branches
-    @fiscal_years = BranchFiscalYearStat.joins(:branch).where( branches: { company_id: current_user.company.id }).distinct.pluck(:fiscal_year)
+    @fiscal_years = BranchFiscalYearStat.joins(:branch).where(branches: { company_id: current_user.company.id }).distinct.pluck(:fiscal_year)
 
     @q = BranchFiscalYearStat.ransack(params[:q])
     @q.sorts = [ "fiscal_year asc", "branch_id asc" ] if @q.sorts.empty?
     @branch_fiscal_year_stats = @q.result(distinct: true)
                                   .joins(:branch)
-                                  .includes(:branch)
+                                  .includes(:branch, :updater)
                                   .where(branches: { company_id: current_user.company.id })
 
     @selection_fiscal_years = @branch_fiscal_year_stats.group_by(&:fiscal_year).keys
@@ -23,10 +23,6 @@ class DashboardsController < ApplicationController
     @total_co2_emissions = @branch_fiscal_year_stats
                             .map(&:calculated_co2_emission)
 
-    # （仮）年度の修正必要
     @branches_without_current_year_stat = @branches.where.not(id: BranchFiscalYearStat.where(fiscal_year: 1.year.ago.year.to_s).pluck(:branch_id))
-
-      # branches_with_current_year_stat =  BranchFiscalYearStat.where(fiscal_year: "2022").pluck(:branch_id)
-
   end
 end
