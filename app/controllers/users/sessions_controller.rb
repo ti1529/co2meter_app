02f2a06b5f8b4bf09f47d986a19ba9493b3b1f05
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-04-24 00:07:08 +0900

    ゲストログイン（一般、管理者）の実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: "ゲストユーザとしてログインしました"
+  end
+
+  def guest_admin_sign_in
+    user = User.guest_admin
+    sign_in user
+    redirect_to root_path, notice: "ゲストユーザ(管理者)としてログインしました"
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
